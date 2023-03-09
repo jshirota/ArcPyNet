0984743e44219ec7a6c72617678422ff8546d4db
Jiro Shirota <jiro.shirota@innovapost.com>
2023-03-09 11:16:55 -0500

    .

--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -17,7 +17,28 @@ public class UnitTest
     [TestMethod]
     public void CreateFileGDB()
     {
-        arcpy.management.CreateFileGDB(arcpy.Workspace, "Test.gdb");
+        arcpy.management.CreateFileGDB(arcpy.Workspace, "Test1.gdb");
+    }
+
+    [TestMethod]
+    public void CreateFeatureclass()
+    {
+        var gdb = "Test2.gdb";
+        var fc = "City";
+        var gdb_path = $@"{arcpy.Workspace}\{gdb}";
+        arcpy.management.CreateFileGDB(arcpy.Workspace, gdb);
+        arcpy.management.CreateFeatureclass(gdb_path, fc, "POINT", null, "DISABLED", "DISABLED", (Code)"arcpy.SpatialReference(4326)");
+
+        var fields = new[]
+        {
+            (name: "Name", type: "TEXT", length: 255),
+            (name: "Population", type: "LONG", length: 0),
+        };
+
+        var code = $"[{string.Join(",", fields.Select(x
+            => $"""["{x.name}","{x.type}","{x.name}",255]"""))}]";
+
+        arcpy.management.AddFields($@"{gdb_path}\{fc}", (Code)code);
     }
 
     record Cat(string Name, int Age);
@@ -37,5 +58,14 @@ public class UnitTest
             var result = arcpy.Run($"{i} * 2");
             Assert.AreEqual(i * 2, result.Evaluate<int>());
         }
+    }
+
+    [TestMethod]
+    public void Evaluate3()
+    {
+        Code code = "arcpy.SpatialReference(4326).exportToString()";
+        var wkt = """GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]];-400 -400 1000000000;-100000 10000;-100000 10000;8.98315284119521E-09;0.001;0.001;IsHighPrecision""";
+
+        Assert.AreEqual(wkt, code.Evaluate<string>());
     }
 }
